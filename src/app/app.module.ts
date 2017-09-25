fe6a27c56655a534fe02be2224edfd74c36a04bd
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-25 00:14:05 -0500

    Rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { app_routing } from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,7 +19,8 @@ import { ItemComponent } from './components/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router'
+
+import { ItemComponent,
+        PortafolioComponent,
+        AboutComponent
+    }from './components/index.paginas'
+
+const app_routes: Routes = [
+    {path:'', component: PortafolioComponent},
+    {path:'item', component: ItemComponent },
+    {path: 'about', component: AboutComponent},
+    {path:'**', pathMatch:'full', redirectTo: ''}
+]
+
+export const app_routing = RouterModule.forRoot(app_routes, {useHash: true})
